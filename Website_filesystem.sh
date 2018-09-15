ce10eca079851c407672492d6169376a10ba1113
Ronald-Latney <ronald.latney@hgs.hiddengeniusproject.org>
2018-09-15 09:51:15 -0700

    successful2

---
+++ Website_filesystem.sh
@@ -0,0 +1,32 @@
+pwd									#Printing working directory
+cd ~/Desktop						#Going to the desktop
+mkdir website-filesystem			#Creates website-filesystem folder
+cd website-filesystem				#Goes into website-filesystem
+touch index.html					#Creates index.html file
+mkdir html							#Creates html folder
+mkdir css							#Creates css folder
+mkdir js							#Creates js folder
+mkdir img							#Creates img folder
+mkdir extra							#Creates extra folder
+cd html								#Goes into html
+touch Pg1.html						#Creates Pg1.html file
+touch pg2.html						#Creates Pg2.html file
+cd ../css							#Goes back and moves to css folder
+touch Pg1.css						#Creates Pg1.css
+touch Pg2.css						#Creates Pg2.css
+touch index.css						#Creates index.css file
+cd ../js							#Goes back and moves to js folder
+touch Pg1.js						#Creates Pg1.js
+touch Pg2.js						#Creates Pg2.js
+touch Pg3.js						#Creates Pg3.js
+cd ../extra							#Goes back and moves to extra folder
+mkdir audio							#Creates audio folder
+mkdir video							#Creates video folder
+cd ../img 							#Goes back and moves to image folder
+
+									#Creates img1.jpg
+curl -o img-1.jpg https://www.thewatchforum.co.uk/uploads/monthly_2016_02/article-2011051-0CDC0FBF00000578-21_306x526.jpg.2c5e44425b0d614bca1aa4c1b13335dc.thumb.jpg.dc6c7c306af14a3a4ba3e8a6e62de159.jpg
+									#Creates img2.jpg
+curl -o img-2.jpg http://literates.co/wp-content/uploads/2018/02/favorable-mural-wallpaper-disney-cars-widescreen-disney-cars-wallpapers-x-for-windows-.jpg
+									#Creates img3.jpg
+curl -o img-3.jpg https://static.makeuseof.com/wp-content/uploads/2013/02/File-Format-Comparison-Featured-670x335.jpg
